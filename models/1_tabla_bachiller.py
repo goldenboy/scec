f6d56e9b1162086fe425fac4b78141bce4254a91
luis diaz <diazluis2007@gmail.com>
2011-07-30 22:16:44 -0430

    + tabla bachiller

--- models/1_tabla_bachiller.py
+++ models/1_tabla_bachiller.py
@@ -5,9 +5,15 @@ db.define_table('baciller',
     Field('nombre', 'string', length=64),
     Field('apellido', 'string', length=64),
     Field('fecha_nac', 'date'),
+    Field('nac', db.pais),
     Field('estado_civil', 'string', length=1),
     Field('sexo', 'string', length=1),
     Field('direccion', 'string', length=512),
     Field('tlf_fijo', 'string', length=64 ),
     Field('tlf_movil', 'string', length=64),
 )
+
+
+
+
+db.baciller.nac.label = 'Nacionalidad'
