443e91524239a676900a022ff987602db6ff91ea
luis diaz <diazluis2007@gmail.com>
2011-07-31 00:35:59 -0430

    + widget tabla bachiller

--- models/1_tabla_bachiller.py
+++ models/1_tabla_bachiller.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+
+
+
 db.define_table('bachiller',
     Field('di', 'integer'),
     Field('nombre', 'string', length=64),
@@ -19,3 +22,6 @@ db.define_table('bachiller',
 db.bachiller.nac.label = 'Nacionalidad'
 db.bachiller.nac.requires = IS_IN_DB(db, 'pais.id', '%(nombre)s', zero='Seleccione el Pais')
 db.bachiller.nac.comment = 'Nacionalidad'
+
+
+db.bachiller.direccion.widget = SQLFORM.widgets.text.widget
